4aa74ec1fcd1ef503f0dccdb953de09f7bd4b540
jbajanam4 <jbajanam4@unemi.edu.ec>
2022-01-09 23:09:35 -0500

    Se subira la tarea

---
+++ Js/constantes.js
@@ -0,0 +1,15 @@
+export const PI = Math.PI;
+
+export let usuario="jon";
+
+const password="qwerty";
+//export default password
+ export default function saludar(){
+     console.log("Hola Modulos +ES6");
+ }
+
+export class Saludar{
+constructor(){
+ console.log("Hola Clases +ES6");
+ }
+}
